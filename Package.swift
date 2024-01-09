39e1d896ac9e898242c91ffc7726b631a0eb187e
Ian Wagner <ian.wagner@stadiamaps.com>
2024-01-09 16:12:42 +0900

    Use proper Swift package for the latest MapLibre prerelease

--- Package.swift
+++ Package.swift
@@ -17,32 +17,25 @@ let package = Package(
         .library(
             name: "MapLibreSwiftDSL",
             targets: ["MapLibreSwiftDSL"]),
-        .library(
-            name: "MapLibre",
-            targets: ["MapLibre"])
     ],
     dependencies: [
 //        .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .upToNextMajor(from: "5.13.0")),
+        .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution.git", from: "6.0.0-pre9599200f2529de44ba62d4662cddb445dc19397d"),
         .package(url: "https://github.com/stadiamaps/maplibre-swift-macros.git", branch: "main")
     ],
     targets: [
-        .binaryTarget(
-            name: "MapLibre",
-            url: "https://github.com/maplibre/maplibre-native/releases/download/ios-v6.0.0-pre9fbcb031f019048f21fdfcb57b80f4451cdecfd9/MapLibre.dynamic.xcframework.zip",
-            checksum: "929bbc3f24740df360bf447acf08bd102cf4baf12a6bc099f42e5fb8b5130cf4"
-        ),
         .target(
             name: "MapLibreSwiftUI",
             dependencies: [
                 .target(name: "InternalUtils"),
                 .target(name: "MapLibreSwiftDSL"),
-                .target(name: "MapLibre"),
+                .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
             ]),
         .target(
             name: "MapLibreSwiftDSL",
             dependencies: [
                 .target(name: "InternalUtils"),
-                .target(name: "MapLibre"),
+                .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
                 .product(name: "MapLibreSwiftMacros", package: "maplibre-swift-macros")
             ]
         ),
