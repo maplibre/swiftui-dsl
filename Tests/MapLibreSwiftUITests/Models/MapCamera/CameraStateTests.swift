113f368b79aaf5ed55c8624d47284d3e33cc1ec8
P W <patrick@subzero.eu>
2024-05-15 15:56:21 +0200

    Pitch improvements (#1)
    * updating tests
    
    * adding pitch setup for initial load
    
    * renaming CameraPitch
    
    * pitch workaround for initial load
    
    * format improvements
    
    * fixing tests
    
    * linting

--- Tests/MapLibreSwiftUITests/Models/MapCamera/CameraStateTests.swift
+++ Tests/MapLibreSwiftUITests/Models/MapCamera/CameraStateTests.swift
@@ -7,26 +7,32 @@ final class CameraStateTests: XCTestCase {
     let coordinate = CLLocationCoordinate2D(latitude: 12.3, longitude: 23.4)
 
     func testCenterCameraState() {
-        let state: CameraState = .centered(onCoordinate: coordinate, zoom: 4, pitch: .free, direction: 42)
-        XCTAssertEqual(state, .centered(onCoordinate: coordinate, zoom: 4, pitch: .free, direction: 42))
+        let state: CameraState = .centered(
+            onCoordinate: coordinate,
+            zoom: 4,
+            pitch: 0,
+            pitchRange: .free,
+            direction: 42
+        )
+        XCTAssertEqual(state, .centered(onCoordinate: coordinate, zoom: 4, pitch: 0, pitchRange: .free, direction: 42))
         assertSnapshot(of: state, as: .description)
     }
 
     func testTrackingUserLocation() {
-        let state: CameraState = .trackingUserLocation(zoom: 4, pitch: .free, direction: 12)
-        XCTAssertEqual(state, .trackingUserLocation(zoom: 4, pitch: .free, direction: 12))
+        let state: CameraState = .trackingUserLocation(zoom: 4, pitch: 0, pitchRange: .free, direction: 12)
+        XCTAssertEqual(state, .trackingUserLocation(zoom: 4, pitch: 0, pitchRange: .free, direction: 12))
         assertSnapshot(of: state, as: .description)
     }
 
     func testTrackingUserLocationWithHeading() {
-        let state: CameraState = .trackingUserLocationWithHeading(zoom: 4, pitch: .free)
-        XCTAssertEqual(state, .trackingUserLocationWithHeading(zoom: 4, pitch: .free))
+        let state: CameraState = .trackingUserLocationWithHeading(zoom: 4, pitch: 0, pitchRange: .free)
+        XCTAssertEqual(state, .trackingUserLocationWithHeading(zoom: 4, pitch: 0, pitchRange: .free))
         assertSnapshot(of: state, as: .description)
     }
 
     func testTrackingUserLocationWithCourse() {
-        let state: CameraState = .trackingUserLocationWithCourse(zoom: 4, pitch: .free)
-        XCTAssertEqual(state, .trackingUserLocationWithCourse(zoom: 4, pitch: .free))
+        let state: CameraState = .trackingUserLocationWithCourse(zoom: 4, pitch: 0, pitchRange: .free)
+        XCTAssertEqual(state, .trackingUserLocationWithCourse(zoom: 4, pitch: 0, pitchRange: .free))
         assertSnapshot(of: state, as: .description)
     }
 
