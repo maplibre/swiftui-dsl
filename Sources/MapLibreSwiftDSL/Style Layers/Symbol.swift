90470d71908daa3f2622f333a9c29d5799878f2e
PW <patrick@subzero.eu>
2024-07-11 11:58:51 +0200

    adding more properties, exposing some private marked properties so extensions can be built, improving compatibility with source based features

--- Sources/MapLibreSwiftDSL/Style Layers/Symbol.swift
+++ Sources/MapLibreSwiftDSL/Style Layers/Symbol.swift
@@ -5,13 +5,23 @@ import MapLibreSwiftMacros
 
 @MLNStyleProperty<Double>("iconRotation", supportsInterpolation: true)
 @MLNStyleProperty<UIColor>("iconColor", supportsInterpolation: true)
+@MLNStyleProperty<Bool>("iconAllowsOverlap", supportsInterpolation: false)
+
 @MLNStyleProperty<UIColor>("textColor", supportsInterpolation: true)
 @MLNStyleProperty<Double>("textFontSize", supportsInterpolation: true)
 @MLNStyleProperty<String>("text", supportsInterpolation: false)
-@MLNStyleProperty<Bool>("iconAllowsOverlap", supportsInterpolation: false)
+// An enum would probably be better?
+@MLNStyleProperty<String>("textAnchor", supportsInterpolation: false)
+@MLNStyleProperty<CGVector>("textOffset", supportsInterpolation: true)
+@MLNStyleProperty<Double>("maximumTextWidth", supportsInterpolation: true)
+
+@MLNStyleProperty<UIColor>("textHaloColor", supportsInterpolation: true)
+@MLNStyleProperty<Double>("textHaloWidth", supportsInterpolation: true)
+@MLNStyleProperty<Double>("textHaloBlur", supportsInterpolation: true)
 
 public struct SymbolStyleLayer: SourceBoundVectorStyleLayerDefinition {
     public let identifier: String
+    public let sourceLayerIdentifier: String?
     public var insertionPosition: LayerInsertionPosition = .aboveOthers
     public var isVisible: Bool = true
     public var maximumZoomLevel: Float? = nil
@@ -22,12 +32,15 @@ public struct SymbolStyleLayer: SourceBoundVectorStyleLayerDefinition {
 
     public init(identifier: String, source: Source) {
         self.identifier = identifier
+        self.sourceLayerIdentifier = nil
         self.source = .source(source)
     }
 
-    public init(identifier: String, source: MLNSource) {
+    public init(identifier: String, source: MLNSource, sourceLayerIdentifier: String? = nil) {
         self.identifier = identifier
+        self.sourceLayerIdentifier = sourceLayerIdentifier
         self.source = .mglSource(source)
+        
     }
 
     public func makeStyleLayer(style: MLNStyle) -> StyleLayer {
@@ -40,10 +53,9 @@ public struct SymbolStyleLayer: SourceBoundVectorStyleLayerDefinition {
         return SymbolStyleLayerInternal(definition: self, mglSource: styleSource)
     }
 
-    // TODO: Other properties and their modifiers
-    fileprivate var iconImageName: NSExpression?
+    public var iconImageName: NSExpression?
 
-    private var iconImages = [UIImage]()
+    public var iconImages = [UIImage]()
 
     // MARK: - Modifiers
 
@@ -52,6 +64,12 @@ public struct SymbolStyleLayer: SourceBoundVectorStyleLayerDefinition {
             it.iconImageName = NSExpression(forConstantValue: image.sha256())
             it.iconImages = [image]
         }
+    }
+    
+    public func iconImage(featurePropertyNamed keyPath: String) -> Self {
+        var copy = self
+        copy.iconImageName = NSExpression(forKeyPath: keyPath)
+        return copy
     }
 
     // FIXME: This appears to be broken upstream; waiting for a new release
@@ -66,6 +84,29 @@ public struct SymbolStyleLayer: SourceBoundVectorStyleLayerDefinition {
 //            it.iconImages = mappings.values + [defaultImage]
 //        }
 //    }
+    
+    /// Add an icon image that can be dynamic and use UIImages in your app, based on a feature property of the source. For example, your feature could have a property called "icon-name". This name is then resolved against the key in the mappings dictionary and used to find a UIImage to display on the map for that feature.
+    /// - Parameters:
+    ///   - keyPath: The keypath to the feature property containing the icon to use, for example "icon-name".
+    ///   - mappings: A lookup dictionary containing the keys found in "keyPath" and a UIImage for each keyPath. The key of the mappings dictionary needs to match the value type stored at keyPath, for example `String`.
+    ///   - defaultImage: A UIImage that MapLibre should fall back to if the key in your feature is not found in the mappings table
+    public func iconImage(featurePropertyNamed keyPath: String, mappings: [AnyHashable: UIImage], default defaultImage: UIImage) -> Self {
+        return modified(self) { it in
+            let attributeExpression = NSExpression(forKeyPath: keyPath)
+            let mappingExpressions = mappings.mapValues { image in
+                NSExpression(forConstantValue: image.sha256())
+            }
+            let mappingDictionary = NSDictionary(dictionary: mappingExpressions)
+            let defaultExpression = NSExpression(forConstantValue: defaultImage.sha256())
+            
+            it.iconImageName = NSExpression(
+                forMLNMatchingKey: attributeExpression,
+                in: mappingDictionary as! [NSExpression: NSExpression],
+                default: defaultExpression
+            )
+            it.iconImages = mappings.values + [defaultImage]
+        }
+    }
 }
 
 private struct SymbolStyleLayerInternal: StyleLayer {
@@ -100,16 +141,24 @@ private struct SymbolStyleLayerInternal: StyleLayer {
 
     public func makeMLNStyleLayer() -> MLNStyleLayer {
         let result = MLNSymbolStyleLayer(identifier: identifier, source: mglSource)
-
+        result.sourceLayerIdentifier = definition.sourceLayerIdentifier
+        
         result.iconImageName = definition.iconImageName
         result.iconRotation = definition.iconRotation
+        result.iconAllowsOverlap = definition.iconAllowsOverlap
         result.iconColor = definition.iconColor
+        
         result.text = definition.text
         result.textColor = definition.textColor
         result.textFontSize = definition.textFontSize
-
-        result.iconAllowsOverlap = definition.iconAllowsOverlap
-
+        result.maximumTextWidth = definition.maximumTextWidth
+        result.textAnchor = definition.textAnchor
+        result.textOffset = definition.textOffset
+        
+        result.textHaloColor = definition.textHaloColor
+        result.textHaloWidth = definition.textHaloWidth
+        result.textHaloBlur = definition.textHaloBlur
+        
         result.predicate = definition.predicate
 
         return result
