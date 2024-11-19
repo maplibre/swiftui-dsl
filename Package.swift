28f287c444f366acafae82b5d7cc1751d2f4763c
Patrick Kladek <patrick.kladek@outlook.com>
2024-11-19 15:34:09 +0800

    swiftformat

--- Package.swift
+++ Package.swift
@@ -54,9 +54,9 @@ let package = Package(
         ),
         .target(
             name: "InternalUtils",
-			dependencies: [
-				.product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
-			],
+            dependencies: [
+                .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
+            ],
             swiftSettings: [
                 .enableExperimentalFeature("StrictConcurrency"),
             ]
