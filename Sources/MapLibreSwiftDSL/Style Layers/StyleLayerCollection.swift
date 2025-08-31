3f63493b2adc7d9fd7ae6582d6deb0f34e8d2efa
Jacob Fielding <jf0517@gmail.com>
2025-08-31 10:28:59 -0700

    style layer collection

--- Sources/MapLibreSwiftDSL/Style Layers/StyleLayerCollection.swift
+++ Sources/MapLibreSwiftDSL/Style Layers/StyleLayerCollection.swift
@@ -3,3 +3,17 @@ import Foundation
 public protocol StyleLayerCollection {
     @MapViewContentBuilder var layers: [StyleLayerDefinition] { get }
 }
+
+/// Enables more natural "Builder" return value (more like SwiftUI's View)
+///
+/// Instead of:
+/// ```
+/// @MapViewContentBuilder mapContent: @escaping () -> [StyleLayerDefinition]
+/// ```
+/// You can do:
+/// ```
+/// @MapViewContentBuilder mapContent: @escaping () -> some StyleLayerCollection
+/// ```
+extension Array: StyleLayerCollection where Element == StyleLayerDefinition {
+    public var layers: [StyleLayerDefinition] { self }
+}
