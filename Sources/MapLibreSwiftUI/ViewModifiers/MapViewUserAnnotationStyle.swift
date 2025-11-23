c745d1d6ad8b02ff31626de2c3b4fde80c1fb6f7
Jacob Fielding <jf0517@gmail.com>
2025-11-23 12:23:08 -0800

    feat: moved user annotation modifier to environment

---
+++ Sources/MapLibreSwiftUI/ViewModifiers/MapViewUserAnnotationStyle.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+private struct MapViewContentUserAnnotationStyleKey: EnvironmentKey {
+    static let defaultValue = MapUserAnnotationStyle()
+}
+
+private extension EnvironmentValues {
+    var mapViewUserAnnotationStyle: MapUserAnnotationStyle {
+        get { self[MapViewContentUserAnnotationStyleKey.self] }
+        set { self[MapViewContentUserAnnotationStyleKey.self] = newValue }
+    }
+}
+
+private struct MapUserAnnotationStyleViewModifier: ViewModifier {
+    let userAnnotationStyle: MapUserAnnotationStyle
+
+    func body(content: Content) -> some View {
+        content.environment(\.mapViewUserAnnotationStyle, userAnnotationStyle)
+    }
+}
+
+public extension View {
+    
+    /// Customize the MapLibre MapView user location annotation style
+    ///
+    /// - Parameter annotationStyle: The customized annotation style.
+    /// - Returns: The modified view heirarchy
+    func mapUserAnnotationStyle(_ annotationStyle: MapUserAnnotationStyle) -> some View {
+        return modifier(MapUserAnnotationStyleViewModifier(userAnnotationStyle: annotationStyle))
+    }
+}
