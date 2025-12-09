745e6a13af9bc7dc2b2ab4ba6baca5aa56a044fb
Jacob Fielding <jf0517@gmail.com>
2025-12-08 21:05:50 -0800

    feat: unsafe modifier to env

--- Sources/MapLibreSwiftUI/MapViewModifiers.swift
+++ Sources/MapLibreSwiftUI/MapViewModifiers.swift
@@ -4,36 +4,6 @@ import MapLibreSwiftDSL
 import SwiftUI
 
 public extension MapView {
-    /// Allows you to set properties of the underlying MLNMapView directly
-    /// in cases where these have not been ported to DSL yet.
-    /// Use this function to modify various properties of the MLNMapView instance.
-    /// For example, you can enable the display of the user's location on the map by setting `showUserLocation` to true.
-    ///
-    /// This is an 'escape hatch' back to the non-DSL world
-    /// of MapLibre for features that have not been ported to DSL yet.
-    /// Be careful not to use this to modify properties that are
-    /// already ported to the DSL, like the camera for example, as your
-    /// modifications here may break updates that occur with modifiers.
-    /// In particular, this modifier is potentially dangerous as it runs on
-    /// EVERY call to `updateUIView`.
-    ///
-    /// - Parameter modifier: A closure that provides you with an MLNMapView so you can set properties.
-    /// - Returns: A MapView with the modifications applied.
-    ///
-    /// Example:
-    /// ```swift
-    ///  MapView()
-    ///     .unsafeMapViewControllerModifier { controller in
-    ///         controller.mapView.showUserLocation = true
-    ///     }
-    /// ```
-    ///
-    func unsafeMapViewControllerModifier(_ modifier: @escaping (T) -> Void) -> MapView {
-        var newMapView = self
-        newMapView.unsafeMapViewControllerModifier = modifier
-        return newMapView
-    }
-
     // MARK: Default Gestures
 
     /// Add an tap gesture handler to the MapView
