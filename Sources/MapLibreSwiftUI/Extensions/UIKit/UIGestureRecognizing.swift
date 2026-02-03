31994c2e1199a5364b5503939327d8cc7150ac66
Jacob Fielding <jf0517@gmail.com>
2026-02-02 17:03:02 -0800

    feat: toggle for developer macros (#131)
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros
    
    * feat: toggle for developer macros

--- Sources/MapLibreSwiftUI/Extensions/UIKit/UIGestureRecognizing.swift
+++ Sources/MapLibreSwiftUI/Extensions/UIKit/UIGestureRecognizing.swift
@@ -1,7 +1,11 @@
-import Mockable
+#if MapLibreDeveloper
+    import Mockable
+#endif
 import UIKit
 
-@Mockable
+#if MapLibreDeveloper
+    @Mockable
+#endif
 public protocol UIGestureRecognizing: AnyObject {
     @MainActor var state: UIGestureRecognizer.State { get }
     @MainActor func location(in view: UIView?) -> CGPoint
